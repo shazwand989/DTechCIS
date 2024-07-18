35e4b74978ce457349826c3b5d35d89d66cdc18f
Izwan Danial <93575615+shazwand989@users.noreply.github.com>
2024-07-18 13:50:31 +0800

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,11 @@
 <?php auth(); ?>
 
 <?php
+// enum('admin', 'cot_officer', 'pkt_management')	
 if (isset($_SESSION['user']) && $_SESSION['user']['user_role'] == 'admin') {
     redirect(base_url('admin/index.php'));
+} else if (isset($_SESSION['user']) && $_SESSION['user']['user_role'] == 'cot_officer') {
+    redirect(base_url('cot-officer/index.php'));
+} else if (isset($_SESSION['user']) && $_SESSION['user']['user_role'] == 'pkt_management') {
+    redirect(base_url('pkt-management/index.php'));
 }
