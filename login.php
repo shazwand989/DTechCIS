c4ca629bbe7f64a767f4fc572e4d88777c3e2831
shazwand27 <93575615+shazwand989@users.noreply.github.com>
2024-07-18 19:53:34 +0800

    Update login.php

--- login.php
+++ login.php
@@ -75,6 +75,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
         }
     }
 }
+if (isset($_SESSION['user']) && $_SESSION['user']['user_role'] == 'admin') {
+    redirect(base_url('admin/index.php'));
+} else if (isset($_SESSION['user']) && $_SESSION['user']['user_role'] == 'cot_officer') {
+    redirect(base_url('cot-officer/index.php'));
+} else if (isset($_SESSION['user']) && $_SESSION['user']['user_role'] == 'pkt_management') {
+    redirect(base_url('pkt-management/index.php'));
+}
 
 ?>
 <!doctype html>
