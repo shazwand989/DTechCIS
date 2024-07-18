ed953ca87e22de1e2014291f8159b4410c11b274
Izwan Danial <93575615+shazwand989@users.noreply.github.com>
2024-07-18 13:47:02 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,7 @@
 <?php require_once 'config/db.php'; ?>
 <?php auth(); ?>
+
+<?php
+if (isset($_SESSION['user']) && $_SESSION['user']['user_role'] == 'admin') {
+    redirect(base_url('admin/index.php'));
+}
